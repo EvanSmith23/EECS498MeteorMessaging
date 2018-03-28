e3ad8c1e8ad286ee81c9e11f6f11582ebab31eea
Evan Smith <smieva@umich.edu>
2018-03-28 13:28:52 -0400

    using reactive variables now

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,12 +5,32 @@ import './message.js';
 import './body.html';
 import './user.html';
 
+Template.body.onCreated(function() {
+  this.login = new ReactiveVar( false );
+  this.loggedInUser = new ReactiveVar("");
+  this.receiver = new ReactiveVar("");
+  this.messages = new ReactiveVar({});
+});
+
 Template.body.helpers({
-  messages() {
-    return Messages.find({}, { sort : { createdAt : -1 } });
+  login: function() {
+    return Template.instance().login.get();
+  },
+  loggedInUser: function() {
+    return Template.instance().loggedInUser.get();
+  },
+  receiver : function () {
+    return Template.instance().receiver.get();
+  },
+  messages : function () {
+    return Messages.find({ $or : [ 
+                                    { sender : Template.instance().loggedInUser.get(), receiver : Template.instance().receiver.get() } , 
+                                    { sender : Template.instance().receiver.get(), receiver : Template.instance().loggedInUser.get() } 
+                                  ] 
+                          },{ sort : { createdAt : -1 }}).fetch();
   },
   users() {
-    return Users.find({});
+    return Users.find({ username : { $ne : Template.instance().loggedInUser.get() } });
   },
 });
 
@@ -25,17 +45,33 @@ Template.body.events({
     const text = target.text.value;
 
     // Insert a task into the collection
-    Messages.insert({
-      text,
-      sender: "Evan",
-      receiver: "Mike",
-      createdAt: new Date(),
-    });
-
+    if( Template.instance().receiver.get() !== "")
+    {
+      Messages.insert({
+        text,
+        sender: Template.instance().loggedInUser.get(),
+        receiver: Template.instance().receiver.get(),
+        createdAt: new Date(),
+      });
+    }
     // Clear form
     target.text.value = "";
   },
-  'click .create-account'(event){
+  'click .user'(event, template){
+    event.preventDefault();
+
+    const target = event.target;
+    template.receiver.set(target.innerHTML);
+  },
+  'click .logout' (event, template){
+
+    event.preventDefault();
+
+    template.receiver.set("");
+    template.loggedInUser.set("");
+    template.login.set(false);
+  },
+  'click .create-account'(event, template){
   
     // stop default event from occurring
     event.preventDefault();
@@ -60,15 +96,12 @@ Template.body.events({
         createdAt: new Date(),
       });
       
-      console.log('here');
+      template.login.set(true);
+      template.loggedInUser.set(username);
 
-      return {
-        login : true,
-      };
-      //document.getElementById('container').innerHTML = '<header><h1>Conversation</h1></header><form class="new-message"><input type="text" name="text" placeholder="new message"><input type="submit" name="submit" value="Send"></form><br/><br/><ul>{{#each messages}}{{> message }}{{/each}}</ul>';      
     }
   },
-  'click .login'(event) {
+  'click .login'(event, template) {
     
     //stop default event form occurring
     event.preventDefault();
@@ -80,14 +113,16 @@ Template.body.events({
     const users = Users.find({ "username" : username }).fetch();
   
     // check the user is logged in
-    if( users )
+    console.log(users);
+    if( users.length > 0 )
     {
-      return document.getElementById('container').innerHTML = '<header><h1>Conversation</h1></header><form class="new-message"><input type="text" name="text" placeholder="new message"><input type="submit" name="submit" value="Send"></form><br/><br/><ul>{{#each messages}}{{> message }}{{/each}}</ul>';      
+      template.login.set(true);
+      template.loggedInUser.set(username);
     } 
     else 
     {
       document.getElementById('errorMessage').innerHTML = "Error: Username does not exist";
-      target.text.value = "";
+      target.value = "";
     }
 
   },
